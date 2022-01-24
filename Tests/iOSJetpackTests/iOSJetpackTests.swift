2cbb23128ab3ee35442581c97f0e36f36af6f362
Yongjie.Xiong <Yongjie.Xiong@geely.com>
2022-01-24 14:08:27 +0800

    Initial Commit

---
+++ Tests/iOSJetpackTests/iOSJetpackTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import iOSJetpack
+
+final class iOSJetpackTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(iOSJetpack().text, "Hello, World!")
+    }
+}
